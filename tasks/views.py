35702eec1921ed992a8eab3e768ad93871d53836
jcastillo2011 <alejandrocastillo2011@gmail.com>
2025-09-06 18:10:35 -0400

    se agrego templete

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Task
+from django.views.generic import ListView #me mpermite crear vistas basadas en clases
 
 # Create your views here.
+
+class TaskListView(ListView):
+    model = Task
+    template_name = 'tasks_list.html'
+   
